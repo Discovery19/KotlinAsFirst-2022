2baafd484208a8eae24fec6baeb12351c981ce8b
Discovery19 <sergeymukiy@gmail.com>
2022-11-28 22:13:31 +0300

    28.11

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -122,7 +122,7 @@ class Tests {
     @Test
     @Tag("2")
     fun containsIn() {
-        assertTrue(containsIn(mapOf(), mapOf()))
+        assertTrue(containsIn(mapOf(), mapOf( )))
         assertTrue(containsIn(mapOf("a" to "z"), mapOf("a" to "z", "b" to "sweet")))
         assertFalse(containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")))
     }
